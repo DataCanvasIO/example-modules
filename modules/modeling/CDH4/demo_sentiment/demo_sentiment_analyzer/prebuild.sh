58da28ec9ce71fe4e0310eebc87a3b4816d13702
Xiaolin Zhang <zhangxl@zetyun.com>
2014-07-13 22:18:51 -0700

    Add prebuild script for demo_sentiment_analyzer

---
+++ modules/modeling/CDH4/demo_sentiment/demo_sentiment_analyzer/prebuild.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+download() {
+    local url=$1
+    local target_file=$2
+    local retry_count=10
+
+    if [ -z "$target_file" ]
+    then
+        target_file=$(basename $url)
+    fi
+    echo "Downloading '$url' to '$target_file'..."
+    for i in `seq 1 $retry_count`; do
+        wget -O $target_file $url && \
+            break || \
+            sleep 10
+    done
+}
+
+##########
+# Main
+##########
+
+echo "Prebuilding for sentiment analysis..."
+
+download https://s3.amazonaws.com/datacanvas-modules/demo_sentiment_analyzer/0.1/HelloAvro-1.0-SNAPSHOT-jar-with-dependencies.jar
