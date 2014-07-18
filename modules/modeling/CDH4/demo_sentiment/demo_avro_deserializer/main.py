511baaf7b47dea4e6a786b71e315aa37d263cca8
Xiaolin Zhang <zhangxl@zetyun.com>
2014-07-18 06:57:45 +0000

    Refactor demo_avro_deserializer's output

--- modules/modeling/CDH4/demo_sentiment/demo_avro_deserializer/main.py
+++ modules/modeling/CDH4/demo_sentiment/demo_avro_deserializer/main.py
@@ -14,7 +14,6 @@ def s3_delete(s3_path, settings):
     if pr.scheme != "s3" and pr.scheme != "s3n":
         raise ValueError("Invalid scheme for path: '%s'" % s3_path)
 
-    import boto
     p = settings.Param
     s3_conn = boto.connect_s3(p.AWS_ACCESS_KEY_ID, p.AWS_ACCESS_KEY_SECRET)
     bucket = s3_conn.get_bucket(pr.netloc)
@@ -25,50 +24,27 @@ def s3_delete(s3_path, settings):
         key.delete()
     return True
 
-def s3_set_metadata(s3_path, settings, metadata):
-    print("s3_set_metadata : %s" % s3_path)
-    from urlparse import urlparse
-    pr = urlparse(s3_path)
-    if pr.scheme != "s3" and pr.scheme != "s3n":
-        raise ValueError("Invalid scheme for path: '%s'" % s3_path)
-
-    import boto
+def get_s3_working_dir(settings, path=""):
+    ps = settings
     p = settings.Param
+    glb_vars = ps.GlobalParam
+
     s3_conn = boto.connect_s3(p.AWS_ACCESS_KEY_ID, p.AWS_ACCESS_KEY_SECRET)
-    bucket = s3_conn.get_bucket(pr.netloc)
-
-    prefix_path = urlparse(s3_path).path[1:]
-    for key in bucket.list(prefix=prefix_path):
-        for meta_key,meta_val in metadata.items():
-            # key.set_metadata(meta_key, meta_val)
-            key.copy(key.bucket, key.name, preserve_acl=True, metadata=metadata)
-
-def s3_set_acl(s3_path, settings, acl):
-    print("s3_set_acl : %s" % s3_path)
-
-    from urlparse import urlparse
-    pr = urlparse(s3_path)
-    if pr.scheme != "s3" and pr.scheme != "s3n":
-        raise ValueError("Invalid scheme for path: '%s'" % s3_path)
-
-    import boto
-    p = settings.Param
-    s3_conn = boto.connect_s3(p.AWS_ACCESS_KEY_ID, p.AWS_ACCESS_KEY_SECRET)
-    bucket = s3_conn.get_bucket(pr.netloc)
-
-    prefix_path = urlparse(s3_path).path[1:]
-    for key in bucket.list(prefix=prefix_path):
-        for acl_key,acl_val in acl.items():
-            if acl_val:
-                key.set_acl(acl_key, acl_val)
-            else:
-                key.set_acl(acl_key)
+    s3_bucket = s3_conn.get_bucket(p.S3_BUCKET)
+    
+    remote_path = os.path.normpath(os.path.join(s3_bucket.name, 'zetjob', glb_vars['userName'], "job%s" % glb_vars['jobId'], "blk%s" % glb_vars['blockId'], path))
+    return os.path.join("s3n://", remote_path)
 
 def main():
     hr = HadoopRuntime("spec.json")
     settings = hr.settings
     print(settings)
 
+    # allocate output_path, and clean it
+    output_path = get_s3_working_dir(settings, "output_path")
+    s3_delete(output_path, settings)
+
+    # build parameters for hadoop job
     jar_file = "avro_tools/hadoop-streaming-2.0.0-mr1-cdh4.6.0.jar"
     hadoop_params = {}
     hadoop_params["HADOOP_MAPRED_HOME"] = "/usr/lib/hadoop-mapreduce"
@@ -94,23 +70,18 @@ def main():
     other_args["mapper"] = "org.apache.hadoop.mapred.lib.IdentityMapper"
     other_args["inputformat"] = "org.apache.avro.mapred.AvroAsTextInputFormat"
     other_args["input"] = settings.Input.avro_path.val
-    other_args["output"] = settings.Param.output_path
+    other_args["output"] = output_path
     other_args_str = " ".join(["-%s %s" % (k,v) for k,v in other_args.items()])
-
-    # TODO: fix this?
-    s3_delete(settings.Param.output_path, settings)
 
     cmd_str = '%s hadoop jar %s %s %s' % (hadoop_params_str, jar_file, jar_defs_str, other_args_str)
     print("Executing:")
     print(cmd_str)
     ret = os.system(cmd_str)
     if ret != 0:
+        print("Job failed")
         sys.exit(ret)
-    
-    output_path = settings.Param.output_path
-    s3_set_metadata(output_path, settings, {"Content-Type" : "application/json"})
-    s3_set_acl(output_path, settings, {"public-read" : None})
 
+    settings.Output.output_path.val = output_path
     print("Done")
 
 if __name__ == "__main__":
--- modules/modeling/CDH4/demo_sentiment/demo_avro_deserializer/test.sh
+++ modules/modeling/CDH4/demo_sentiment/demo_avro_deserializer/test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "hdfs://10.10.0.173/tmp/zetjob/xiaolin/job456/blk789/sentiment_result" > input.avro_path
+echo "hdfs://10.10.0.87/tmp/zetjob/admin/job99/blk380/sentiment_result" > input.avro_path
+touch output.s3_path
 
 screwjack run docker \
-    --param-hdfs_root="hdfs://10.10.0.173" \
-    --param-AWS_ACCESS_KEY_ID="" \
-    --param-AWS_ACCESS_KEY_SECRET="" \
-    --param-output_path="s3n://xiaolin/review_sentiment_results" \
-    --param-yarn_resourcemanager="10.10.0.173:8032" \
-    --param-yarn_resourcemanager_scheduler="10.10.0.173:8030" \
-    --avro_path=input.avro_path
+    --param-hdfs_root="hdfs://10.10.0.87" \
+    --param-AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+    --param-AWS_ACCESS_KEY_SECRET="$AWS_SECRET_ACCESS_KEY" \
+    --param-yarn_resourcemanager="10.10.0.87:8032" \
+    --param-yarn_resourcemanager_scheduler="10.10.0.87:8030" \
+    --avro_path=input.avro_path \
+    --output_path=output.s3_path \
+    --param-S3_BUCKET="xiaolin"
