751735569db6b429844bd998ba34a459fe482173
JackyGuo1 <leon.k.scott@hotmail.com>
2014-11-14 02:54:16 +0000

    upgrade hero_cos_matc_spark

--- modules/modeling/spark/hero_cos_matc_spark/main.py
+++ modules/modeling/spark/hero_cos_matc_spark/main.py
@@ -8,8 +8,10 @@ from specparser import HadoopRuntime
 def main():
     hr = HadoopRuntime()
     settings = hr.settings
-    output_dir = hr.get_hdfs_working_dir("some_path")
-    settings.Output.match_result.val = output_dir
+    match_result_output_dir = hr.get_hdfs_working_dir("match_result")
+    settings.Output.match_result.val = match_result_output_dir
+    match_analysis_output_dir = hr.get_hdfs_working_dir("match_analysis")
+    settings.Output.match_analysis.val = match_analysis_output_dir
 
 #SPARK_HOME=/home/run/spark-1.1.0-bin-cdh4 
 #/home/run/spark_word_segement.jar
@@ -20,7 +22,13 @@ def main():
 --num-executors 3 --driver-memory 1024m  --executor-memory 1024m   --executor-cores 1 \
 --conf "spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:MaxPermSize=1024m" \
 /home/run/spark_word_segement.jar \
-%s %s %s '''  %(settings.Param.spark_host,settings.Input.jd_dir.val,settings.Input.rs_dir.val,settings.Output.match_result.val ))
+%s %s %s %s %s '''  %(settings.Param.spark_host,
+                      settings.Input.jd_dir.val,
+                      settings.Input.rs_dir.val,
+                      settings.Output.match_result.val,
+                      settings.Output.match_analysis.val,
+                      settings.Input.white_dict.val
+                      ))
     print("Done")
 
 if __name__ == "__main__":
