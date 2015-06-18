ab52c89d6965c8ec731b89ca18780ecd47281e5d
Xiaolin Zhang <zhangxl@zetyun.com>
2015-06-18 18:02:40 +0800

    Refactor datasource_to_hdfs to new RM style

--- modules/data_source/datasource_to_hdfs/main.py
+++ modules/data_source/datasource_to_hdfs/main.py
@@ -3,94 +3,99 @@
 
 import sys
 import json
-from specparser import HadoopRuntime, cmd
+import sarge
+from datacanvas.new_runtime import DataCanvas
+from datacanvas.io_types import DS_HDFS
+from datacanvas.utils import mask_key
+dc = DataCanvas(__name__)
 
-def upload_s3_to_hdfs(hr, settings, ds):
+def check_returncodes(ret_codes):
+    print("exit code = %s" % ret_codes)
+    return all(v == 0 for v in ret_codes)
 
-    # Prepare working directory
-    hr.hdfs_clean_working_dir()
+def upload_s3_to_hdfs(rt, ds):
 
-    output_dir = hr.get_hdfs_working_dir("dest_hdfs")
-
-    AWS_ACCESS_KEY_ID = ds["Meta"]["key"]
-    AWS_SECRET_ACCESS_KEY = ds["Meta"]["token"]
+    output_dir = rt.get_working_dir("dest_hdfs")
 
     hadoop_params = {}
-    hadoop_params["HADOOP_MAPRED_HOME"] = "/usr/lib/hadoop-mapreduce"
+    # hadoop_params["HADOOP_MAPRED_HOME"] = "/usr/lib/hadoop-mapreduce"
     hadoop_params_str = " ".join(["%s=%s" % (k,v) for k,v in hadoop_params.items()])
 
     jar_defs = {}
-    jar_defs["fs.s3n.awsAccessKeyId"] = '"%s"' % AWS_ACCESS_KEY_ID
-    jar_defs["fs.s3n.awsSecretAccessKey"] = '"%s"' % AWS_SECRET_ACCESS_KEY
-    jar_defs["fs.s3.awsAccessKeyId"] = '"%s"' % AWS_ACCESS_KEY_ID
-    jar_defs["fs.s3.awsSecretAccessKey"] = '"%s"' % AWS_SECRET_ACCESS_KEY
-    jar_defs["mapreduce.framework.name"] = "yarn"
-    jar_defs["yarn.resourcemanager.address"] = settings.Param.yarn_resourcemanager
-    jar_defs["yarn.resourcemanager.scheduler.address"] = settings.Param.yarn_resourcemanager_scheduler
-    jar_defs["fs.defaultFS"] = settings.Param.hdfs_root
+
+    if "key" in ds["Meta"] and "token" in ds["Meta"]:
+        AWS_ACCESS_KEY_ID = ds["Meta"]["key"]
+        AWS_SECRET_ACCESS_KEY = ds["Meta"]["token"]
+        jar_defs["fs.s3n.awsAccessKeyId"] = '"%s"' % AWS_ACCESS_KEY_ID
+        jar_defs["fs.s3n.awsSecretAccessKey"] = '"%s"' % AWS_SECRET_ACCESS_KEY
+        jar_defs["fs.s3.awsAccessKeyId"] = '"%s"' % AWS_ACCESS_KEY_ID
+        jar_defs["fs.s3.awsSecretAccessKey"] = '"%s"' % AWS_SECRET_ACCESS_KEY
+
+    sec_keys = ["fs.s3n.awsAccessKeyId", "fs.s3n.awsSecretAccessKey",
+                "fs.s3.awsAccessKeyId", "fs.s3.awsSecretAccessKey"]
+
+    jar_defs_sec = { k: mask_key(v) if k in sec_keys else v for k,v in jar_defs.items() }
+
     jar_defs_str = " ".join(["-D %s=%s" % (k,v) for k,v in jar_defs.items()])
-
+    jar_defs_sec_str = " ".join(["-D %s=%s" % (k,v) for k,v in jar_defs_sec.items()])
     cmd_str = '%s hadoop distcp %s %s %s' % (hadoop_params_str, jar_defs_str, ds['URL'], output_dir)
+    cmd_str_sec = '%s hadoop distcp %s %s %s' % (hadoop_params_str, jar_defs_sec_str, ds['URL'], output_dir)
     print("Executing:")
-    print(cmd_str)
-    ret = cmd(cmd_str)
-    print("exit code = %d" % ret)
-    if ret != 0:
-        sys.exit(ret)
+    print(cmd_str_sec)
 
-    return {
-        "Name": "Dummy datasource : %s" % output_dir,
-        "Type": "HDFS",
-        "URL": output_dir,
-        "Meta": {}
-    }
+    ret = sarge.run(cmd_str, env=rt.cluster.env_vars)
+    if not check_returncodes(ret.returncodes):
+        sys.exit(-1)
 
-def upload_url_to_hdfs(hr, settings, ds):
-    # Prepare working directory
-    hr.hdfs_clean_working_dir()
-    output_dir = hr.get_hdfs_working_dir("dest_hdfs")
+    return DS_HDFS(URL=output_dir)
+
+def upload_url_to_hdfs(rt, ds):
+    output_dir = rt.get_working_dir("dest_hdfs")
 
     hadoop_params = {}
     hadoop_params_str = " ".join(["%s=%s" % (k,v) for k,v in hadoop_params.items()])
 
     jar_defs = {}
-    jar_defs["fs.defaultFS"] = settings.Param.hdfs_root
     jar_defs_str = " ".join(["-D %s=%s" % (k,v) for k,v in jar_defs.items()])
 
     cmd_str = 'wget -q -O - %s | hadoop fs -put - %s' % (ds["URL"], output_dir)
     print("Executing:")
     print(cmd_str)
-    ret = cmd(cmd_str)
-    print("exit code = %d" % ret)
-    if ret != 0:
-        sys.exit(ret)
 
-    return {
-        "Name": "Dummy datasource : %s" % output_dir,
-        "Type": "HDFS",
-        "URL": output_dir,
-        "Meta": {}
-    }
+    ret = sarge.run(cmd_str, env=rt.cluster.env_vars)
+    if not check_returncodes(ret.returncodes):
+        sys.exit(-1)
 
-def main():
-    hr = HadoopRuntime("spec.json")
-    settings = hr.settings
-    print(settings)
-
-    ds = json.load(open(settings.Input.DS))
+    return DS_HDFS(URL=output_dir)
 
 
+@dc.hadoop_runtime(spec_json="spec.json")
+def mymodule(rt, params, inputs, outputs):
+    import datacanvas
+    print datacanvas.__version__
+    jar_step_args = []
+
+    # TODO: Execute your jar
+    # Method 1 : Execute local jar
+    # rt.execute_jar("./your_project.jar", jar_step_args)
+    # Method 2 : Execute jar in s3
+    # rt.execute_jar("s3n://your_bucket/your_project.jar", jar_step_args)
+
+    ds = inputs.DS.val
     if ds['Type'] in ["Http", "LocalFile", "Http", "Ftp"]:
-        ds_output = upload_url_to_hdfs(hr, settings, ds)
+        ds_output = upload_url_to_hdfs(rt, ds)
     elif ds['Type'] in ["AWS_S3"]:
-        ds_output = upload_s3_to_hdfs(hr, settings, ds)
+        ds_output = upload_s3_to_hdfs(rt, ds)
     elif ds['Type'] in ["HDFS"]:
         ds_output = ds
     else:
         raise ValueError("Invalid type for input datasource: '%s'" % ds['Type'])
 
-    settings.Output.dest_hdfs.val = json.dumps(ds_output)
+    print ds
+    print ds_output
+    outputs.dest_hdfs.val = ds_output
     print("Done")
 
+
 if __name__ == "__main__":
-    main()
+    dc.run()
--- modules/data_source/datasource_to_hdfs/test.sh
+++ modules/data_source/datasource_to_hdfs/test.sh
@@ -1,30 +1,71 @@
 #!/bin/bash
 
+# export PYTHONPATH=/home/xiaolin/work/zetyun/pyDataCanvas.git/
+
 set -x
 
+TEST_CLUSTER=playground_cdh4
+
+# EXEC_METHOD=local
+# EXEC_METHOD=docker
+EXEC_METHOD=docker-machine
+TEST_CLUSTER="${TEST_CLUSTER:-hdp_sandbox}"
+
+
+###############
+# Test Case 1
+###############
+screwjack gen_ds \
+    --ds_type=hdfs \
+    --ds_url=hdfs://10.10.0.162/tmp/movielens/big/ratings.csv \
+    > input2.ds.json
+
+screwjack run ${EXEC_METHOD} \
+    --param-cluster=${TEST_CLUSTER} \
+    --DS=input2.ds.json \
+    --dest_hdfs=output.hdfs && \
+    cat output.hdfs
+
+###############
+# Test Case 2
+###############
+screwjack gen_ds \
+    --ds_type=http \
+    --ds_url=http://nlp.stanford.edu/courses/NAACL2013/NAACL2013-Socher-Manning-DeepLearning.pdf \
+    > input2.ds.json
+
+screwjack run ${EXEC_METHOD} \
+    --param-cluster=${TEST_CLUSTER} \
+    --DS=input2.ds.json \
+    --dest_hdfs=output.hdfs && \
+    cat output.hdfs
+
+
+###############
+# Test Case 3
+###############
 # screwjack gen_ds \
 #     --ds_type=s3 \
 #     --ds_url=s3n://xiaolin/movielens/big/ratings.csv \
 #     key=$AWS_ACCESS_KEY_ID \
 #     token=$AWS_SECRET_ACCESS_KEY > input2.ds.json
 
-# screwjack gen_ds \
-#     --ds_type=http \
-#     --ds_url=http://nlp.stanford.edu/courses/NAACL2013/NAACL2013-Socher-Manning-DeepLearning.pdf \
-#     > input2.ds.json
-
+# WITH aws_key/aws_sec
 screwjack gen_ds \
-    --ds_type=hdfs \
-    --ds_url=hdfs://10.10.0.162/tmp/movielens/big/ratings.csv \
+    --ds_type=s3 \
+    --ds_url=s3n://datacanvas-opendata/movielens/medium/ratings.csv \
+    key=$AWS_ACCESS_KEY_ID \
+    token=$AWS_SECRET_ACCESS_KEY \
     > input2.ds.json
 
-touch output.hdfs
+# # WITHOUT aws_key/aws_sec
+# screwjack gen_ds \
+#     --ds_type=s3 \
+#     --ds_url=s3n://datacanvas-opendata/movielens/medium/ratings.csv \
+#     > input2.ds.json
 
-screwjack run docker \
-    --param-hdfs_root="hdfs://10.10.0.162" \
-    --param-yarn_resourcemanager="10.10.0.162:8032" \
-    --param-yarn_resourcemanager_scheduler="10.10.0.162:8030" \
+screwjack run ${EXEC_METHOD} \
+    --param-cluster=${TEST_CLUSTER} \
     --DS=input2.ds.json \
     --dest_hdfs=output.hdfs && \
     cat output.hdfs
-
